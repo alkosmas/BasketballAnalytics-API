256b65a9620471b89ac8d90b23a150be2351f9c3
Alex Kosmas <alkosmas.92@gmail.com>
2025-11-13 19:26:14 +0200

    Connection with Postgres and Migration

--- BasketballAnalytics.Api/Program.cs
+++ BasketballAnalytics.Api/Program.cs
@@ -6,7 +6,6 @@ using BasketballAnalytics.Persistence.DbContext;
 using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.InMemory;
 
 
 
@@ -14,7 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("BasketballDb"));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 builder.Services.AddMediatR(cfg =>
 {
@@ -41,17 +42,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-      using (var scope = app.Services.CreateScope())
-    {
-        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        context.Database.EnsureCreated(); //it will create base if it hasn't have base 
-        if (!context.Teams.Any())
-        {
-            context.Teams.Add(new BasketballAnalytics.Domain.Entities.Team { Id = Guid.NewGuid(), Name = "Olympiacos", City = "Piraeus" });
-            context.Teams.Add(new BasketballAnalytics.Domain.Entities.Team { Id = Guid.NewGuid(), Name = "Panathinaikos", City = "Athens" });
-            context.SaveChanges();
-        }
-    }
 }
 
 app.UseHttpsRedirection();
